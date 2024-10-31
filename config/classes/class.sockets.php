37793ffe2fb8c0635dd5bd20a468a748f905dd9d
Barry Dick <barryd.it@gmail.com>
2024-10-31 15:37:40 -0700

    muted proc_get_statue()

--- config/classes/class.sockets.php
+++ config/classes/class.sockets.php
@@ -290,7 +290,7 @@ END
 
         $process = popen((defined('PHP_EXEC') ? PHP_EXEC : dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bin/psexec.exe -d C:\xampp\php\php.exe -f ') . APP_PATH . 'server.php', "r");
         (is_resource($process)) and
-          file_put_contents($pidFile, $pid = proc_get_status($process)['pid']);
+          file_put_contents($pidFile, $pid = @proc_get_status($process)['pid']);
     }
 
     public static function getInstance()
